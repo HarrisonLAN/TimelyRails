b1cc84f58641c04ab036fdeb19fa5f8665837949
HarrisonLAN <robharrison0399@gmail.com>
2023-07-19 21:03:27 +0100

    Initial commit with api endpoint

---
+++ app/api/getDepartures/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function POST(req : NextRequest){
+  const body = await req.json();
+  const res = await fetch('http://localhost:3001/api/getLDB/Departures/Board?CRS='+body.crs)
+  const services = await res.json();
+  return new NextResponse(JSON.stringify(services))
+}
